9d93a9adfd40f8bf66eecef73aaa9ad232445e99
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-02-21 17:33:38 +0100

    Remove deprecated attributes from Poll::Question::Answer
    Some fields from Poll::Question::Answer are translatable and we no
    longer need them. This commit will remove the annoying deprecation
    warning thrown by Globalize gem after gem version update.

--- db/schema.rb
+++ db/schema.rb
@@ -1017,8 +1017,6 @@ ActiveRecord::Schema.define(version: 20190429125842) do
   end
 
   create_table "poll_question_answers", force: :cascade do |t|
-    t.string  "title"
-    t.text    "description"
     t.integer "question_id"
     t.integer "given_order", default: 1
     t.boolean "most_voted",  default: false
